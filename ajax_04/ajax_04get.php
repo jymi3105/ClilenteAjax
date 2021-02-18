569958431c089a60465da50a39738d0372f33e4d
Jymi3105 <jymi3105@hotmail.com>
2021-02-18 18:28:58 +0100

    DD

--- ajax_04/ajax_04get.php
+++ ajax_04/ajax_04get.php
@@ -1,5 +1,5 @@
 <?php
-	mysql_set_charset('utf8');
+	/*mysql_set_charset('utf8');*/
 	echo " -- MARCA: " . $_GET["marca"];
 	echo " -- MODELO: " . $_GET["modelo"];
 ?>
--- ajax_13/ajax_13.php
+++ ajax_13/ajax_13.php
@@ -5,15 +5,15 @@
 	$password = "jymi";
 
 	try {
-		//mysql_set_charset('utf8');
 		$conn = new PDO("mysql:host=$servername;dbname=$dbName;charset=utf8", $username, $password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$titulo = $_GET["texto"]; 
-		$resultados = $conn->query("select titulo from libros where titulo like '" . $titulo . "%' order by titulo");
-		$fila = $resultados->fetch(PDO::FETCH_ASSOC);
-		echo json_encode($fila["titulo"]);
-
+		if(isset($_GET["editorial"])) {
+			$editorial = $_GET["editorial"];
+			foreach($conn->query("SELECT * FROM libros where editorial ='" . $editorial . "'") as $fila) {
+				echo $fila["titulo"];
+			}
+		}
 	} catch(PDOException $e) {
 		echo "Fallo la conexión: " . $e->getMessage();
 	}
